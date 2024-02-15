a39ee3ceeb8e646932c8e554c3d4d1dabd15b2b6
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-02-15 22:38:14 +0100

    First test, first fixes.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 install() {
-  sudo apt-get install -y "$1"
+  sudo apt-get install -q -y "$1" &> /dev/null && echo "Successfully installed $1"
+}
+
+clone() {
+  sudo git clone --depth 1 "$1" "$2" &> /dev/null
 }
 
 update() {
-  sudo apt-get update
+  sudo apt-get update &> /dev/null
 }
 
 upgrade() {
-  sudo apt-get upgrade
+  sudo apt-get upgrade &> /dev/null
 }
 
 config_file="$HOME/.bashrc"
@@ -28,47 +32,35 @@ update && upgrade
 
 install "curl wget"
 
-echo "Networking utilities have been successfully installed [curl, wget]"
-
 ### Archives (Tools)
 
 install "tar gzip unzip"
-
-echo "Archive utilities have been successfully installed [tar, gzip, unzip]"
 
 ### Manuals (Tools)
 
 install "man man-db manpages-dev"
 
-echo "Manuals have been successfully installed [man man-db manpages-dev]"
-
 ### Development toolchain (Tools) 
 
 install "git ssh ssh-keygen"
-
-echo "Git toolchain has been successfully installed [git, ssh, ssh-keygen]"
 
 ### C/C++ Toolchain (Tools)
 
 install "gcc g++ gdb make cmake"
 
-echo "C/C++ toolchain has been successfully installed [gcc, g++, gdb, make, cmake]"
-
 ### GF [GDB Frontend] (Tools)
 
 install "x11lib-dev python3 python3-venv"
 
-echo "GF dependencies have been successfully installed [X11, python3, python3-venv]"
+clone "https://github.com/nakst/gf.git" "$third_party_software_dir/gf"
 
-git clone https://github.com/nakst/gf.git $third_party_software_dir/gf
-
-bash $third_party_software_dir/gf/build.sh
+source "$third_party_software_dir/gf/build.sh"
 
 echo "GF has been successfully installed"
 
 ### Neovim [Superior text editor] (Tools)
 
-cd $third_party_software_dir
+cd "$third_party_software_dir"
 
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 
@@ -84,15 +76,15 @@ echo "Neovim has been successfully installed"
 
 ### Fuzzy finder [File search extension] (Terminal)
 
-git clone --depth 1 https://github.com/junegunn/fzf.git "$third_party_software_dir/fzf"
+clone "https://github.com/junegunn/fzf.git" "$third_party_software_dir/fzf"
 
-bash "$third_party_software_dir/fzf/install"
+source "$third_party_software_dir/fzf/install"
 
 echo "Fuzzy finder has been successfully installed"
 
 ### Zoxide [Better terminal navigation] (Terminal)
 
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | source
 
 eval "$(zoxide init --cmd cd bash)" >> $config_file
 
@@ -100,4 +92,4 @@ echo "Zoxide has been successfully installed"
 
 ### Sync config file with the changes
 
-source $config_file
+source "$config_file"
